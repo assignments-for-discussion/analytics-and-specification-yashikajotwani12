6bd792de0647c6e22f289a927c11d5d81566c6f2
Yashika Jotwani <77020164+yashikajotwani12@users.noreply.github.com>
2021-12-07 20:09:24 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,7 +1,7 @@
 
 function average(numbers) {
-  const newArray = numbers.filter( value => !Number.isNaN(value) );
-  return newArray.reduce((a, b)=> a + b, 0) / newArr.length;
+   const newArr = numbers.filter( value => !Number.isNaN(value) );
+  return newArr.reduce((p, c)=> p + c, 0) / newArr.length;
 }
 
 module.exports = {average};
